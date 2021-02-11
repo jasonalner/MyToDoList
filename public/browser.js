0406468d183af9f549e659659bc4a8e455798dde
usermane66 <68332445+usermane66@users.noreply.github.com>
2021-02-11 11:16:46 +0000

    adding browser files

---
+++ public/browser.js
@@ -0,0 +1,11 @@
+document.addEventListener("click", function(e) {
+    if (e.target.classList.contains("edit-me")) {
+        let userInput = prompt("Enter your desired new text") 
+        axios.post('/update-item', {text: userInput}).then( function() {
+            //do something interesting here
+        }).catch( function() {
+            console.log("please try again later")
+        })
+    }
+})
+
